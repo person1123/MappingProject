83407936697564b2f186e80e2a2eb63f40ef2855
RafaelUL <rafaelulima@gmail.com>
2014-11-06 22:59:11 -0500

    Finished DCCStudent class
    Finished both compare functions, added comments

--- DCCStudent.py
+++ DCCStudent.py
@@ -13,38 +13,106 @@ class DCCStudent(object):
 			self.majorList.append(major2)
 
 	def getMajorList(self):
+		# returns a list containing all of the student's majors
 		return self.majorList
 	
 	def getMajor1(self):
+		# returns student's major1
 		return self.major1
 
 	def getMajor2(self):
+		# returns student's major2
 		return self.major2
 
 	def getMinor(self):
+		# returns student's minor
 		return self.minor
 
-	def compareTo(self, otherStudent):
-		
-		#WIP
+	# Compares both students majors and returns a value.
+	def compareMajor(self, otherStudent):
 
 		matchVal = 0.0
 
+		# Iterates through every major and their descriptions
+		# Descriptions are lists detailing the department/college and the kind of the major
+		# Descriptions are entries on a general major dictionary, with the major as key
 		for major in self.majorList:
 			for otherMajor in otherStudent.getMajorList:
 				
 				majorDesc = majorDict[major]
 				otherMajorDesc = majorDict[otherMajor]
 
-				if majorDesc[0] == otherMajorDesc[0]:
+				# Triggers if both majors are the same
+				if major == otherMajor:
 					matchVal += 2
 
 				else:
 
-					if majorDesc[1] == otherMajorDesc[1]:
+					# Triggers if both majors are in the same department/college
+					if majorDesc[0] == otherMajorDesc[0]:
 						matchVal += 1.5
 
 					else:
 
-						if majorDesc[2] == otherMajorDesc[2]:
+						# Triggers if both majors are of the same kind (eg Humanities)
+						if majorDesc[1] == otherMajorDesc[1]:
+							matchVal += 1
+
+		return matchVal
+
+	# Compares both students minors and returns a value.
+	def compareMinor(self, otherStudent):
 		
+		matchVal = 0.0
+
+		# Triggers if student has a minor
+		if self.minor != "NMD":
+			minorDesc = majorDict[self.minor]
+
+			# Iterates through otherStudent's majors and their descriptions
+			for otherMajor in otherStudent.getMajorList:
+
+				otherMajorDesc = majorDict[otherMajor]
+
+				# Triggers if minor and otherStudent's major are the same
+				if minor == otherMajor:
+					matchVal += 1
+
+				else:
+
+					# Triggers if minor and otherStudent's major are in the same college/department
+					if minorDesc[0] == otherMajorDesc[0]:
+						matchVal += 075
+
+					else:
+
+						# Triggers if minor and otherStudent's major are of the same kind
+						if minorDesc[1] == otherMajorDesc[1]:
+							matchVal += .5
+
+		# Triggers if otherStudent has a minor
+		if otherStudent.minor != "NMD":
+			otherMinorDesc = majorDict[otherStudent.minor]
+
+			# Iterates through Student's majors and their descriptions
+			for major in self.getMajorList:
+
+				majorDesc = majorDict[mjor]
+
+				# Triggers if otherStudent's minor and Student's major are the same
+				if otherMinor == major:
+					matchVal += 1
+
+				else:
+
+					# Triggers if otherStudent's minor and Student's major are in the same college/department
+					if otherMinorDesc[0] == majorDesc[0]:
+						matchVal += 075
+
+					else:
+
+						# Triggers if otherStudent's minor and Student's major are of the same kind
+						if otherMinorDesc[1] == majorDesc[1]:
+							matchVal += .5
+		
+		return matchVal
