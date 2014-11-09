75874f871d5dfacbf5a18b847f8ec42ebf4993c2
RafaelUL <rafaelulima@gmail.com>
2014-11-09 17:43:27 -0500

    Actually finished DCCStudent class and majorDict
    Added compare interests, classes and a general comparison method.
    
    Finished dictionary of majors

--- DCCStudent.py
+++ DCCStudent.py
@@ -1,6 +1,6 @@
 class DCCStudent(object):
 	"""docstring for DCCStudent"""
-	def __init__(self, firstName, lastName, major1, major2, minor, dccClass, floor):
+	def __init__(self, firstName, lastName, major1, major2, minor, dccClass, floor, interestList, classesList):
 		super(DCCStudent, self).__init__()
 		self.firstName = firstName
 		self.lastName = lastName
@@ -8,8 +8,10 @@ class DCCStudent(object):
 		self.minor = minor
 		self.dccClass = dccClass
 		self.floor = floor
+		self.interestList = interestList
+		self.classList = classList
 
-		if major2 != noMajor:
+		if major2 != "noMajor":
 			self.majorList.append(major2)
 
 	def getMajorList(self):
@@ -28,91 +30,139 @@ class DCCStudent(object):
 		# returns student's minor
 		return self.minor
 
-	# Compares both students majors and returns a value.
+	def getInterestList(self):
+		# returns student's interestList
+		return self.interestList
+
+	def getClassList(self):
+		# returns student's classList
+		return self.classList
+
+	# Compares both students' majors and returns a value.
 	def compareMajor(self, otherStudent):
 
-		matchVal = 0.0
+		majorMatchVal = 0.0
 
 		# Iterates through every major and their descriptions
 		# Descriptions are lists detailing the department/college and the kind of the major
 		# Descriptions are entries on a general major dictionary, with the major as key
 		for major in self.majorList:
-			for otherMajor in otherStudent.getMajorList:
+			for otherMajor in otherStudent.getMajorList():
 				
 				majorDesc = majorDict[major]
 				otherMajorDesc = majorDict[otherMajor]
 
 				# Triggers if both majors are the same
 				if major == otherMajor:
-					matchVal += 2
+					majorMatchVal += 2
 
 				else:
 
 					# Triggers if both majors are in the same department/college
 					if majorDesc[0] == otherMajorDesc[0]:
-						matchVal += 1.5
+						majorMatchVal += 1.5
 
 					else:
 
 						# Triggers if both majors are of the same kind (eg Humanities)
 						if majorDesc[1] == otherMajorDesc[1]:
-							matchVal += 1
+							majorMatchVal += 1
 
-		return matchVal
+		return majorMatchVal
 
-	# Compares both students minors and returns a value.
+	# Compares both students' minors and returns a value.
 	def compareMinor(self, otherStudent):
 		
-		matchVal = 0.0
+		minorMatchVal = 0.0
 
 		# Triggers if student has a minor
 		if self.minor != "NMD":
 			minorDesc = majorDict[self.minor]
 
 			# Iterates through otherStudent's majors and their descriptions
-			for otherMajor in otherStudent.getMajorList:
+			for otherMajor in otherStudent.getMajorList():
 
 				otherMajorDesc = majorDict[otherMajor]
 
 				# Triggers if minor and otherStudent's major are the same
 				if minor == otherMajor:
-					matchVal += 1
+					minorMatchVal += 1
 
 				else:
 
 					# Triggers if minor and otherStudent's major are in the same college/department
 					if minorDesc[0] == otherMajorDesc[0]:
-						matchVal += 0.75
+						minorMatchVal += 0.75
 
 					else:
 
 						# Triggers if minor and otherStudent's major are of the same kind
 						if minorDesc[1] == otherMajorDesc[1]:
-							matchVal += .5
+							minorMatchVal += .5
+
+			# Triggers if both minors are declared and the same
+			if self.minor == otherStudent.getMinor():
+				minorMatchVal += 1
 
 		# Triggers if otherStudent has a minor
-		if otherStudent.minor != "NMD":
-			otherMinorDesc = majorDict[otherStudent.minor]
+		if otherStudent.getMinor() != "NMD":
+			otherMinorDesc = majorDict[otherStudent.getMinor()]
 
 			# Iterates through Student's majors and their descriptions
-			for major in self.getMajorList:
+			for major in self.majorList:
 
-				majorDesc = majorDict[mjor]
+				majorDesc = majorDict[major]
 
 				# Triggers if otherStudent's minor and Student's major are the same
 				if otherMinor == major:
-					matchVal += 1
+					minorMatchVal += 1
 
 				else:
 
 					# Triggers if otherStudent's minor and Student's major are in the same college/department
 					if otherMinorDesc[0] == majorDesc[0]:
-						matchVal += 075
+						minorMatchVal += 075
 
 					else:
 
 						# Triggers if otherStudent's minor and Student's major are of the same kind
 						if otherMinorDesc[1] == majorDesc[1]:
-							matchVal += .5
+							minorMatchVal += .5
 		
+		return minorMatchVal
+
+	# Compares both students' interests and returns a value
+	def compareInterest(self, otherStudent):
+		
+		interestMatchVal = 0.0
+
+		for interest in self.interestList:
+			for otherInterest in otherStudent.getInterestList():
+				
+				if interest == otherInterest:
+					interestMatchVal += 1
+
+		return interestMatchVal
+
+	# Compares both students' classes and returns a value
+	def compareClasses(self, otherStudent):
+
+		classMatchVal = 0.0
+
+		for studentClass in self.classList:
+			for otherClass in otherStudent.getClassList():
+				
+				if studentClass == otherClass:
+					classMatchVal += 1
+
+		return classMatchVal
+
+	# Compares both students
+	def compareTo(self, otherStudent):
+		
+		matchVal = compareMajor(self, otherStudent)
+		matchVal += compareMinor(self, otherStudent)
+		matchVal += compareInterest(self, otherStudent)
+		matchVal += compareClasses(self, otherStudent)
+
 		return matchVal
--- majorDict.py
+++ majorDict.py
@@ -1,3 +1,7 @@
-majorDict = {"Aerospace Engineering": [], "Art history": [], "Astronomy": [], "Computer Engineering": [], "Computer Science": [], "Economics": [], 
-"Electrical Engineering": [], "English": [], "Finance": [], "Fire Protection Engineering": [], "Materials Engineering": [], "Materials Science": [], 
-"Mechanical Engineering": [], "Neurobiology": [], "Physics": [], "Physiology": [], "Psychology": [], "Studio Art": [], "Supply Chain": []}
+majorDict = {"Aerospace Engineering": ["Engineering", "Exact"], "Art history": ["ARHU", "Humanities"], "Astronomy": ["CMNS", "Exact"], 
+"Computer Engineering": ["Engineering", "Exact"], "Computer Science": ["CMNS", "Exact"], "Economics": ["BSOS", "Humanities"], 
+"Electrical Engineering": ["Engineering", "Exact"], "English": ["ARHU", "Humanities"], "Finance": ["Business", "Humanities"], 
+"Fire Protection Engineering": ["Engineering", "Exact"], "Materials Science and Engineering": ["Engineering", "Exact"], 
+"Mechanical Engineering": ["Engineering", "Exact"], "Neurobiology": ["Medicine", "Biological"], "Physics": ["CMNS", "Exact"], 
+"Physiology": ["Medicine", "Biological"], "Psychology": ["BSOS", "Humanities"], "Studio Art": ["ARHU", "Humanities"], 
+"Supply Chain Management": ["Business", "Humanities"]}
