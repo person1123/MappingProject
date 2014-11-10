92772ac06d022c3f45873f249b3b4d762991fa3e
RafaelUL <rafaelulima@gmail.com>
2014-11-10 17:43:58 -0500

    Lowercase

--- majorDict.py
+++ majorDict.py
@@ -1,7 +1,7 @@
-majorDict = {"Aerospace Engineering": ["Engineering", "Exact"], "Art history": ["ARHU", "Humanities"], "Astronomy": ["CMNS", "Exact"], 
-"Computer Engineering": ["Engineering", "Exact"], "Computer Science": ["CMNS", "Exact"], "Economics": ["BSOS", "Humanities"], 
-"Electrical Engineering": ["Engineering", "Exact"], "English": ["ARHU", "Humanities"], "Finance": ["Business", "Humanities"], 
-"Fire Protection Engineering": ["Engineering", "Exact"], "Materials Science and Engineering": ["Engineering", "Exact"], 
-"Mechanical Engineering": ["Engineering", "Exact"], "Neurobiology": ["Medicine", "Biological"], "Physics": ["CMNS", "Exact"], 
-"Physiology": ["Medicine", "Biological"], "Psychology": ["BSOS", "Humanities"], "Studio Art": ["ARHU", "Humanities"], 
-"Supply Chain Management": ["Business", "Humanities"]}
+majorDict = {"aerospace engineering": ["Engineering", "Exact"], "art history": ["ARHU", "Humanities"], "astronomy": ["CMNS", "Exact"], 
+"computer engineering": ["Engineering", "Exact"], "computer science": ["CMNS", "Exact"], "economics": ["BSOS", "Humanities"], 
+"electrical engineering": ["Engineering", "Exact"], "english": ["ARHU", "Humanities"], "finance": ["Business", "Humanities"], 
+"fire protection engineering": ["Engineering", "Exact"], "materials science and engineering": ["Engineering", "Exact"], 
+"mechanical engineering": ["Engineering", "Exact"], "neurobiology": ["Medicine", "Biological"], "physics": ["CMNS", "Exact"], 
+"physiology": ["Medicine", "Biological"], "psychology": ["BSOS", "Humanities"], "studio art": ["ARHU", "Humanities"], 
+"supply chain": ["Business", "Humanities"], "undecided": ["nope", "nope"]}
