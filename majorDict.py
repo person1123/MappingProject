50dc5c5b1db6a2440eacede348f7e123de6b1b4b
RafaelUL <rafaelulima@gmail.com>
2014-11-10 17:51:59 -0500

    Added math to dict

--- majorDict.py
+++ majorDict.py
@@ -1,7 +1,7 @@
 majorDict = {"aerospace engineering": ["Engineering", "Exact"], "art history": ["ARHU", "Humanities"], "astronomy": ["CMNS", "Exact"], 
 "computer engineering": ["Engineering", "Exact"], "computer science": ["CMNS", "Exact"], "economics": ["BSOS", "Humanities"], 
 "electrical engineering": ["Engineering", "Exact"], "english": ["ARHU", "Humanities"], "finance": ["Business", "Humanities"], 
-"fire protection engineering": ["Engineering", "Exact"], "materials science and engineering": ["Engineering", "Exact"], 
+"fire protection engineering": ["Engineering", "Exact"], "materials science and engineering": ["Engineering", "Exact"], "math": ["CMSN", "Exact"],
 "mechanical engineering": ["Engineering", "Exact"], "neurobiology": ["Medicine", "Biological"], "physics": ["CMNS", "Exact"], 
 "physiology": ["Medicine", "Biological"], "psychology": ["BSOS", "Humanities"], "studio art": ["ARHU", "Humanities"], 
 "supply chain": ["Business", "Humanities"], "undecided": ["nope", "nope"]}
